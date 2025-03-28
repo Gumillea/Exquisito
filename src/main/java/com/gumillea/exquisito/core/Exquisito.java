c4cbccbc72fe62f97c01fe8219c091a35cb77605
Gumillea <ea2171785569@outlook.com>
2025-03-28 22:12:29 +0800

    1.20.1

--- src/main/java/com/gumillea/exquisito/core/Exquisito.java
+++ src/main/java/com/gumillea/exquisito/core/Exquisito.java
@@ -23,6 +23,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -31,6 +33,7 @@ import java.util.concurrent.CompletableFuture;
 public class Exquisito
 {
     public static final String MODID = "exquisito";
+    public static final Logger LOGGER = LogManager.getLogger();
     public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MODID);
     public Exquisito()
     {
